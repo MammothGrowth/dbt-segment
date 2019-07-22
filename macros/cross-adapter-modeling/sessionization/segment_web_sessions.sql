dee4c1638fab3c9376f9abdff8637b951435161c
Drew Beaupre <drew@beauprefamily.org>
2019-07-22 15:58:00 -0700

    removing more safe_cast

--- macros/cross-adapter-modeling/sessionization/segment_web_sessions.sql
+++ macros/cross-adapter-modeling/sessionization/segment_web_sessions.sql
@@ -17,12 +17,10 @@
 {% set sessionization_cutoff = "
 (
     select {{
-        dbt_utils.safe_cast(
             dbt_utils.dateadd(
                 'hour',
                 -var('segment_sessionization_trailing_window'),
-                'max(session_start_tstamp)'),
-            'timestamp') }}
+                'max(session_start_tstamp)') }}
     from {{this}}
 )    
 " %}
